801d739dabadee318373ec0066fdfaf054a06b2c
Hanalulux <130019767+Hanalulux@users.noreply.github.com>
2023-04-05 22:57:38 -0400

    added test source

---
+++ src/ManhwaX/ManhwaX.ts
@@ -0,0 +1,44 @@
+import {
+    ContentRating,
+    SourceInfo,
+    BadgeColor,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://manhwax.org'
+
+export const ManhwaXInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'ManhwaX',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.jpg',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [
+        {
+            text: 'Notifications',
+            type: BadgeColor.GREEN
+        },
+        {
+            text: '18+',
+            type: BadgeColor.YELLOW
+        }
+    ],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class ManhwaX extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override alternativeChapterAjaxEndpoint = false
+
+    override hasAdvancedSearchPage = true
+}
