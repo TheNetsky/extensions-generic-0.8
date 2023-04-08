84710ec6df2da6a8b8150851647197a409e45738
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-04-08 15:47:17 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic-0.8@a71ff256d1bd7b3090c0f3baf46e4d4de1afd03c 🚀

--- buddycomplex/MangaBuddy/index.js
+++ buddycomplex/MangaBuddy/index.js
@@ -1403,11 +1403,22 @@ class BuddyComplexParser {
             const date = new Date($('time.chapter-update', chapter)?.text() ?? '');
             if (!id)
                 continue;
-            const getNumber = id.split('-').pop() ?? '';
-            const chapterNumberRegex = getNumber.match(/(\d+\.?\d?)+/);
+            const chapterRegex = id.match(/chapter-(\d+)(?:[-.]\d+)?/);
+            let chapterNumberRegex;
+            if (chapterRegex) {
+                chapterNumberRegex = chapterRegex[0].match(/(\d+)(?:[-.]\d+)?/);
+            }
+            else {
+                const getNumber = id.split('-').pop() ?? '';
+                chapterNumberRegex = getNumber.match(/(\d+)(?:[-.]\d+)?/);
+            }
             let chapterNumber = 0;
-            if (chapterNumberRegex && chapterNumberRegex[1])
-                chapterNumber = Number(chapterNumberRegex[1]);
+            if (chapterNumberRegex && chapterNumberRegex[1]) {
+                let chapterRegex = chapterNumberRegex[1];
+                if (chapterRegex.includes("-"))
+                    chapterRegex = chapterRegex.replace("-", ".");
+                chapterNumber = Number(chapterRegex);
+            }
             chapters.push({
                 id: id,
                 name: title,
