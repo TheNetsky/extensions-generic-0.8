36ecd12277982ca1fd0ef9bde424b54ed911d53f
Nicholas Lim <nicknitewolf@lineageos.org>
2023-05-04 23:07:57 +0800

    PianManga: Use toongod homepage format here too

--- src/PianManga/PianManga.ts
+++ src/PianManga/PianManga.ts
@@ -2,7 +2,10 @@ import {
     ContentRating,
     SourceInfo,
     BadgeColor,
-    SourceIntents
+    SourceIntents,
+    PagedResults,
+    PartialSourceManga,
+    HomeSection
 } from '@paperback/types'
 
 import {
@@ -34,4 +37,107 @@ export class PianManga extends Madara {
     override hasAdvancedSearchPage = true
 
     override usePostIds = false
+
+    override async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        const sections = [
+            {
+                request: App.createRequest({
+                    url: `${this.baseUrl}/manga?m_orderby=latest`,
+                    method: 'GET'
+                }),
+                section: App.createHomeSection({
+                    id: '0',
+                    title: 'Recently Updated',
+                    type: 'singleRowNormal',
+                    containsMoreItems: true
+                }),
+            },
+            {
+                request: App.createRequest({
+                    url: `${this.baseUrl}/manga?m_orderby=views`,
+                    method: 'GET'
+                }),
+                section: App.createHomeSection({
+                    id: '1',
+                    title: 'Most Popular',
+                    type: 'singleRowNormal',
+                    containsMoreItems: true
+                })
+            },
+            {
+                request: App.createRequest({
+                    url: `${this.baseUrl}/manga?m_orderby=new-manga`,
+                    method: 'GET'
+                }),
+                section: App.createHomeSection({
+                    id: '2',
+                    title: 'New Manga',
+                    type: 'singleRowNormal',
+                    containsMoreItems: true
+                })
+            }
+        ]
+
+        const promises: Promise<void>[] = []
+        for (const section of sections) {
+            // Let the app load empty sections
+            sectionCallback(section.section)
+
+            // Get the section data
+            promises.push(
+                this.requestManager.schedule(section.request, 1).then(async response => {
+                    this.CloudFlareError(response.status)
+                    const $ = this.cheerio.load(response.data as string)
+                    section.section.items = await this.parser.parseHomeSection($, this)
+                    sectionCallback(section.section)
+                }),
+            )
+
+        }
+
+        // Make sure the function completes
+        await Promise.all(promises)
+    }
+
+    override async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        const page = metadata?.page ?? 1
+        let param: string
+
+        switch (homepageSectionId) {
+            case '0': {
+                param = 'm_orderby=latest'
+                break
+            }
+            case '1': {
+                param = 'm_orderby=views'
+                break
+            }
+            case '2': {
+                param = 'm_orderby=new-manga'
+                break
+            }
+            default:
+                throw new Error(`Invalid homeSectionId | ${homepageSectionId}`)
+        }
+
+        const request = App.createRequest({
+            url: `${this.baseUrl}/manga/page/${page}/?${param}`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        this.CloudFlareError(response.status)
+        const $ = this.cheerio.load(response.data as string)
+        const items: PartialSourceManga[] = await this.parser.parseHomeSection($, this)
+
+        let mData: any = { page: (page + 1) }
+        if (!$('a.last')) {
+            mData = undefined
+        }
+
+        return App.createPagedResults({
+            results: items,
+            metadata: mData
+        })
+    }
 }
