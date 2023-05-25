ac21debf7154e13833d958f8fac3868b1ba67f9c
Netsky <56271887+TheNetsky@users.noreply.github.com>
2023-05-25 23:24:31 +0200

    HiperDex | Update domain

--- src/HiperDex/HiperDex.ts
+++ src/HiperDex/HiperDex.ts
@@ -10,7 +10,7 @@ import {
     Madara
 } from '../Madara'
 
-const DOMAIN = 'https://hiperdex.net'
+const DOMAIN = 'https://hiperdex.com'
 
 export const HiperDexInfo: SourceInfo = {
     version: getExportVersion('0.0.2'),
