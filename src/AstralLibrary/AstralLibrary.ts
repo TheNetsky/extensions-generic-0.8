a1ae3f7db986a8068eb667c618d88bbd58e7ea67
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-08-13 01:55:36 +0200

    Add Astral Library

---
+++ src/AstralLibrary/AstralLibrary.ts
@@ -0,0 +1,34 @@
+import {
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://astrallibrary.net'
+
+export const AstralLibraryInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'AstralLibrary',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class AstralLibrary extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override alternativeChapterAjaxEndpoint = false
+
+    override hasAdvancedSearchPage = true
+}
