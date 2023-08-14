c98be68d992d65a9e9231c0cca1975c2e3edc426
MacLeod Bracken <59837039+macbrack@users.noreply.github.com>
2023-08-14 18:13:39 -0500

    Updated hiperdex sources from dot net to dot xyz. (#38)
    * Updated hiperdex sources from dot net to dot xyz.
    
    * Increased version number.

--- src/HiperDex/HiperDex.ts
+++ src/HiperDex/HiperDex.ts
@@ -10,10 +10,10 @@ import {
     Madara
 } from '../Madara'
 
-const DOMAIN = 'https://hiperdex.com'
+const DOMAIN = 'https://hiperdex.xyz'
 
 export const HiperDexInfo: SourceInfo = {
-    version: getExportVersion('0.0.2'),
+    version: getExportVersion('0.0.3'),
     name: 'HiperDex',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
