80eee7c44c202993ce09050f28ada1300aece33f
Ivanmatthew <76107136+Ivanmatthew@users.noreply.github.com>
2023-08-17 18:45:18 +0200

    Add second source with chapter protector plugin on
    To ensure it doesn't work for just one source.

---
+++ src/ManhuaSY/ManhuaSY.ts
@@ -0,0 +1,40 @@
+import {
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://manhuasy.com'
+
+export const ManhuaSYInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'ManhuaSY',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Community',
+    authorWebsite: '-',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class ManhuaSY extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override alternativeChapterAjaxEndpoint = true
+
+    override hasAdvancedSearchPage = true
+
+    override hasProtectedChapters = true
+
+    override usePostIds = false
+
+    override directoryPath = 'manhua'
+}
