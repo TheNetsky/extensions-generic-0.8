a1b396c9bfff51d1b74fdd12f99bfd49dcc6e2e6
seitenca <alexdltg@protonmail.com>
2023-08-19 20:26:36 +0200

    Add NightScans

---
+++ src/NightScans/NightScans.ts
@@ -0,0 +1,35 @@
+import {
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://nightscans.org/'
+
+export const NightScansInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'NightScans',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: []
+}
+
+export class NightScans extends MangaStream {
+
+    baseUrl: string = DOMAIN
+
+    override configureSections() {
+        this.homescreen_sections['latest_update'].selectorFunc = ($: CheerioStatic) => $('div.bsx', $('h2:contains(Latest Update)')?.parent()?.next())
+        this.homescreen_sections['latest_update'].subtitleSelectorFunc = ($: CheerioStatic, element: CheerioElement) => $('a.maincl', element).first().text().trim()
+    }
+}
