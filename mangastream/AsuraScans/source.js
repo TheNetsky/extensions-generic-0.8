0a7b200f0e02bcfefc77cab8ebd1dde4e9eb7fc8
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-09-02 21:22:13 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic-0.8@dcf288c04a099958b16acf2b57fc43bf9c0458dd 🚀

--- mangastream/AsuraScans/source.js
+++ mangastream/AsuraScans/source.js
@@ -1438,9 +1438,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AsuraScans = exports.AsuraScansInfo = void 0;
 const types_1 = require("@paperback/types");
 const MangaStream_1 = require("../MangaStream");
-const DOMAIN = 'https://asura.nacm.xyz';
+const DOMAIN = 'https://asuracomics.com';
 exports.AsuraScansInfo = {
-    version: (0, MangaStream_1.getExportVersion)('0.0.3'),
+    version: (0, MangaStream_1.getExportVersion)('0.0.4'),
     name: 'AsuraScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
