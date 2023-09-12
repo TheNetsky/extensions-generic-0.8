25a4a7ae3b38b2acda33bd0913b4a366832ed14e
Netsky <56271887+TheNetsky@users.noreply.github.com>
2023-09-12 23:18:48 +0200

    HiperDex | Update Domain

--- src/HiperDex/HiperDex.ts
+++ src/HiperDex/HiperDex.ts
@@ -10,10 +10,10 @@ import {
     Madara
 } from '../Madara'
 
-const DOMAIN = 'https://hiperdex.com'
+const DOMAIN = 'https://hiperdex.xyz'
 
 export const HiperDexInfo: SourceInfo = {
-    version: getExportVersion('0.0.4'),
+    version: getExportVersion('0.0.5'),
     name: 'HiperDex',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
