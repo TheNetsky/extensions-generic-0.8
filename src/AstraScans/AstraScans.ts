eee4cb7330b8e53ec6296ac1e8e23b82fe860934
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-10-07 19:34:49 +0200

    Add AstraScans

---
+++ src/AstraScans/AstraScans.ts
@@ -0,0 +1,35 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://astrascans.com'
+
+export const AstraScansInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'AstraScans',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: []
+}
+
+export class AstraScans extends MangaStream {
+
+    baseUrl: string = DOMAIN
+
+    override configureSections() {
+        this.homescreen_sections['new_titles'].enabled = false
+    }
+}
