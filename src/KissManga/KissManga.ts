72b0c993f60d9bb39a8936b588492ffcb5f1db4a
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-11-06 22:05:25 +0100

    Revert "Remove 1stkissmanga"
    This reverts commit 939a3f8619aa3e0d0e277829b482bbe2c1048b23.

---
+++ src/KissManga/KissManga.ts
@@ -0,0 +1,35 @@
+import {
+    ContentRating,
+    SourceInfo,
+    BadgeColor,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://1stkissmanga.me'
+
+export const KissMangaInfo: SourceInfo = {
+    version: getExportVersion('0.0.1'),
+    name: 'KissManga',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class KissManga extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override alternativeChapterAjaxEndpoint = true
+
+    override usePostIds = false
+}
