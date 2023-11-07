94b1a131de7f442a0f0320d89fd81525ba4c5186
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2023-11-07 20:05:50 +0100

    Update formatting

--- src/HelScans/HelScans.ts
+++ src/HelScans/HelScans.ts
@@ -1,26 +1,34 @@
-import { BadgeColor, ContentRating, SourceInfo, SourceIntents, Request } from "@paperback/types";
+import {
+	BadgeColor,
+	ContentRating,
+	SourceInfo,
+	SourceIntents
+} from '@paperback/types'
 
-import { getExportVersion, MangaStream } from "../MangaStream";
+import {
+	getExportVersion,
+	MangaStream
+} from '../MangaStream'
 
-const DOMAIN = "https://helscans.com";
+const DOMAIN = 'https://helscans.com'
 
 export const HelScansInfo: SourceInfo = {
-	version: getExportVersion("0.0.0"),
-	name: "HelScans",
+	version: getExportVersion('0.0.0'),
+	name: 'HelScans',
 	description: `Extension that pulls manga from ${DOMAIN}`,
-	author: "Netsky",
-	authorWebsite: "http://github.com/TheNetsky",
-	icon: "icon.png",
+	author: 'Netsky',
+	authorWebsite: 'http://github.com/TheNetsky',
+	icon: 'icon.png',
 	contentRating: ContentRating.MATURE,
 	websiteBaseURL: DOMAIN,
 	intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
-	sourceTags: [],
-};
+	sourceTags: []
+}
 
 export class HelScans extends MangaStream {
 	baseUrl: string = DOMAIN
 
 	override configureSections(): void {
-		this.homescreen_sections["new_titles"].enabled = false;
+		this.homescreen_sections['new_titles'].enabled = false
 	}
 }
