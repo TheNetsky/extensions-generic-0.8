930401f4846cf892f89f3aea63ce918ecf686380
Nacinra <10849122+Nacinra@users.noreply.github.com>
2023-11-21 01:33:28 +0100

    [Add] MangaReadOrg

---
+++ src/MangaReadOrg/MangaReadOrg.ts
@@ -0,0 +1,38 @@
+import {
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+import { 
+    MangaReadOrgParser
+ } from './MangaReadOrgParser'
+
+const DOMAIN = 'https://www.mangaread.org'
+
+export const MangaReadOrgInfo : SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'MangaReadOrg',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class MangaReadOrg extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override alternativeChapterAjaxEndpoint = true
+
+    override parser: MangaLekParser = new MangaReadOrgParser()
+}
---
+++ src/MangaReadOrg/MangaReadOrgParser.ts
@@ -0,0 +1,40 @@
+import {
+    Parser
+} from '../MadaraParser'
+
+export class MangaReadOrgParser extends Parser {
+
+    parseDate = (date: string): Date => {
+        date = date.toUpperCase()
+        let time: Date
+        const number = Number((/\d*/.exec(date) ?? [])[0])
+        if (date.includes('LESS THAN AN HOUR') || date.includes('JUST NOW')) {
+            time = new Date(Date.now())
+        } else if (date.includes('YEAR') || date.includes('YEARS')) {
+            time = new Date(Date.now() - (number * 31556952000))
+        } else if (date.includes('MONTH') || date.includes('MONTHS')) {
+            time = new Date(Date.now() - (number * 2592000000))
+        } else if (date.includes('WEEK') || date.includes('WEEKS')) {
+            time = new Date(Date.now() - (number * 604800000))
+        } else if (date.includes('YESTERDAY')) {
+            time = new Date(Date.now() - 86400000)
+        } else if (date.includes('DAY') || date.includes('DAYS')) {
+            time = new Date(Date.now() - (number * 86400000))
+        } else if (date.includes('HOUR') || date.includes('HOURS')) {
+            time = new Date(Date.now() - (number * 3600000))
+        } else if (date.includes('MINUTE') || date.includes('MINUTES') || date.includes('MINS')) {
+            time = new Date(Date.now() - (number * 60000))
+        } else if (date.includes('SECOND') || date.includes('SECONDS')) {
+            time = new Date(Date.now() - (number * 1000))
+        } else if (date.includes('.')){
+            const dateParts = date.split('.')
+            const year = parseInt(dateParts[2])
+            const month = parseInt(dateParts[1]) - 1
+            const day = parseInt(dateParts[0])
+            time = new Date(year, month, day)
+        } else {
+            time = new Date(date)
+        }
+        return time
+    }
+}
