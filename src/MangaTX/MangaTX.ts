308862c8056758c42c2e806f6990d2fb7dff26fe
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-03-05 20:06:54 +0100

    Re-add MangaTX

---
+++ src/MangaTX/MangaTX.ts
@@ -0,0 +1,33 @@
+import {
+    ContentRating,
+    SourceInfo,
+    BadgeColor,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://mangatx.to'
+
+export const MangaTXInfo: SourceInfo = {
+    version: getExportVersion('0.0.2'),
+    name: 'MangaTX',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class MangaTX extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override chapterEndpoint = 1
+}
