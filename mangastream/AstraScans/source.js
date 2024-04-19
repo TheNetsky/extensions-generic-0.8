a55e530f6b2c27e430b50e563377acd35935c324
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-04-19 19:34:08 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic-0.8@f8a517ed5368061da625331b9b901867eabbf4e2 🚀

--- mangastream/AstraScans/source.js
+++ mangastream/AstraScans/source.js
@@ -1438,9 +1438,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AstraScans = exports.AstraScansInfo = void 0;
 const types_1 = require("@paperback/types");
 const MangaStream_1 = require("../MangaStream");
-const DOMAIN = 'https://astrascans.com';
+const DOMAIN = 'https://astrascans.org';
 exports.AstraScansInfo = {
-    version: (0, MangaStream_1.getExportVersion)('0.0.0'),
+    version: (0, MangaStream_1.getExportVersion)('0.0.1'),
     name: 'AstraScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
