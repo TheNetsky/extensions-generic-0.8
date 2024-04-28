3f9cf1db588392739cab821457bebfca0c23b86a
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-04-28 16:16:16 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic-0.8@3afcc3791057ed047b831947fbafa14d806b8db5 🚀

--- madara/MangaClash/index.js
+++ madara/MangaClash/index.js
@@ -9643,9 +9643,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.MangaClash = exports.MangaClashInfo = void 0;
 const types_1 = require("@paperback/types");
 const Madara_1 = require("../Madara");
-const DOMAIN = 'https://mangaclash.com';
+const DOMAIN = 'https://toonclash.com';
 exports.MangaClashInfo = {
-    version: (0, Madara_1.getExportVersion)('0.0.0'),
+    version: (0, Madara_1.getExportVersion)('0.0.1'),
     name: 'MangaClash',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
