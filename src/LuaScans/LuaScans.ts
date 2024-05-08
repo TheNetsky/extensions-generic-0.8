e0def6e61857f820374ba0a9b762fb3f720591ab
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-05-09 00:03:31 +0200

    Add 3 Sources
    - Added CypherScans
    - Added LuaScans
    - Added MangaGalaxy

---
+++ src/LuaScans/LuaScans.ts
@@ -0,0 +1,35 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://luascans.com'
+
+export const LuaScansInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'LuaScans',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: []
+}
+
+export class LuaScans extends MangaStream {
+
+    baseUrl: string = DOMAIN
+
+    override configureSections() {
+        this.homescreen_sections['new_titles'].enabled = false
+    }
+}
---
+++ src/MangaGalaxy/MangaGalaxy.ts
@@ -0,0 +1,37 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    MangaStream
+} from '../MangaStream'
+
+const DOMAIN = 'https://mangagalaxy.me'
+
+export const MangaGalaxyInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'MangaGalaxy',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI,
+    sourceTags: []
+}
+
+export class MangaGalaxy extends MangaStream {
+
+    baseUrl: string = DOMAIN
+
+    override directoryPath = 'series'
+
+    override configureSections() {
+        this.homescreen_sections['new_titles'].enabled = false
+    }
+}
