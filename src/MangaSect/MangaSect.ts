88e049951fd8db13131c909fc9396ad59b751612
xOnlyFadi <fadewailyosefptoo@gmail.com>
2024-05-27 16:27:10 -0600

    New Generic Liliana

---
+++ src/MangaSect/MangaSect.ts
@@ -0,0 +1,31 @@
+import {
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    Liliana,
+    getExportVersion
+} from '../Liliana'
+
+const DOMAIN = 'https://mangasect.net'
+
+export const MangaSectInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'Manga Sect',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'xOnlyFadi',
+    authorWebsite: 'http://github.com/xOnlyFadi',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    websiteBaseURL: DOMAIN,
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED,
+    sourceTags: []
+}
+
+export class MangaSect extends Liliana {
+
+    baseUrl: string = DOMAIN
+    override usesPostSearch = true
+}
