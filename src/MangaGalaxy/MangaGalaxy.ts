0a8c14839dcd791a0ec01bf34621818d8baa379e
Ivan Matthew <76107136+Ivanmatthew@users.noreply.github.com>
2024-06-08 18:32:25 +0200

    Fix stray chapters arising with MangaGalaxy (#96)
    * Implement logic for handling unconventional chapter notation
    
    As only MangaGalaxy has this, it is only applied for MangaGalaxy.
    
    * Update version

--- src/MangaGalaxy/MangaGalaxy.ts
+++ src/MangaGalaxy/MangaGalaxy.ts
@@ -1,5 +1,5 @@
 import {
-    BadgeColor,
+    ChapterDetails,
     ContentRating,
     SourceInfo,
     SourceIntents
@@ -9,11 +9,13 @@ import {
     getExportVersion,
     MangaStream
 } from '../MangaStream'
+import { MangaStreamParser } from '../MangaStreamParser'
+import { MangaGalaxyParser } from './MangaGalaxyParser'
 
 const DOMAIN = 'https://mangagalaxy.me'
 
 export const MangaGalaxyInfo: SourceInfo = {
-    version: getExportVersion('0.0.0'),
+    version: getExportVersion('0.1.0'),
     name: 'MangaGalaxy',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
@@ -33,5 +35,59 @@ export class MangaGalaxy extends MangaStream {
 
     override configureSections() {
         this.homescreen_sections['new_titles'].enabled = false
+    }
+
+    override parser: MangaStreamParser = new MangaGalaxyParser()
+
+    override async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
+        // Request the manga page
+        const request = App.createRequest({
+            url: await this.getUsePostIds() ? `${this.baseUrl}/?p=${mangaId}/` : `${this.baseUrl}/${this.directoryPath}/${mangaId}/`,
+            method: 'GET'
+        })
+
+        const response = await this.requestManager.schedule(request, 1)
+        this.checkResponseError(response)
+        const $ = this.cheerio.load(response.data as string)
+
+        let chapter = $('div#chapterlist').find('li[data-num="' + chapterId + '"]')
+        if (chapter.length === 0) {
+            // final attempt is to search for the chapter number in the chapter title, and also only list the ones wi thout data-num
+            const chapters = $('div#chapterlist li:not([data-num]),li[data-num=""]').toArray()
+
+            for (let i = 0; i < chapters.length; i++) {
+                // @ts-expect-error Always defined.
+                const matches = $('a', chapters[i])
+                if (matches.length === 0) {
+                    continue
+                }
+
+                if (matches.attr('href')?.includes(chapterId)) {
+                    chapter = $(chapters[i])
+                    break
+                }
+            }
+
+            if (chapter.length === 0) {
+                throw new Error(`Unable to fetch a chapter for chapter number: ${chapterId}`)
+            }
+        }
+
+        // Fetch the ID (URL) of the chapter
+        const id = $('a', chapter).attr('href') ?? ''
+        if (!id) {
+            throw new Error(`Unable to fetch id for chapter number: ${chapterId}`)
+        }
+        // Request the chapter page
+        const _request = App.createRequest({
+            url: id,
+            method: 'GET'
+        })
+
+        const _response = await this.requestManager.schedule(_request, 1)
+        this.checkResponseError(_response)
+        const _$ = this.cheerio.load(_response.data as string)
+
+        return this.parser.parseChapterDetails(_$, mangaId, chapterId)
     }
 }
