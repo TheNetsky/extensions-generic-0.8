ad8d4ec15be84c4d2d6ecd3c96ee6f5217911bf2
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-07-02 00:10:33 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic-0.8@a2f7df46842fe1a20bcd1403a417321bb0e6dc50 🚀

--- mangastream/ElarcPage/source.js
+++ mangastream/ElarcPage/source.js
@@ -1438,9 +1438,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ElarcPage = exports.ElarcPageInfo = void 0;
 const types_1 = require("@paperback/types");
 const MangaStream_1 = require("../MangaStream");
-const DOMAIN = 'https://elarctoons.com';
+const DOMAIN = 'https://elarctoons.ink';
 exports.ElarcPageInfo = {
-    version: (0, MangaStream_1.getExportVersion)('0.0.2'),
+    version: (0, MangaStream_1.getExportVersion)('0.0.3'),
     name: 'ElarcPage',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
