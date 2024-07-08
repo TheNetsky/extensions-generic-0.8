7131fa7064bb6a0144665763e46e6efc156a1cb5
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-07-08 21:50:58 +0000

    Deploying to gh-pages from @ TheNetsky/extensions-generic-0.8@4d5c4fc6c1bdb17167c1f2f67d697652f0785ec0 🚀

--- madara/ResetScans/source.js
+++ madara/ResetScans/source.js
@@ -9158,9 +9158,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ResetScans = exports.ResetScansInfo = void 0;
 const types_1 = require("@paperback/types");
 const Madara_1 = require("../Madara");
-const DOMAIN = 'https://resetscan.com';
+const DOMAIN = 'https://reset-scans.co';
 exports.ResetScansInfo = {
-    version: (0, Madara_1.getExportVersion)('0.0.3'),
+    version: (0, Madara_1.getExportVersion)('0.0.4'),
     name: 'ResetScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
