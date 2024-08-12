cfb7e2407bf5463ae1ee70ff9dfde26e0bc0009c
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2024-08-13 00:54:03 +0200

    Add TheBlank

---
+++ src/TheBlank/TheBlank.ts
@@ -0,0 +1,38 @@
+import {
+    BadgeColor,
+    ContentRating,
+    SourceInfo,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://theblank.net'
+
+export const TheBlankInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'TheBlank',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'http://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.ADULT,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [
+        {
+            text: '18+',
+            type: BadgeColor.YELLOW
+        }
+    ],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class TheBlank extends Madara {
+
+    baseUrl: string = DOMAIN
+
+    override chapterEndpoint = 1
+}
