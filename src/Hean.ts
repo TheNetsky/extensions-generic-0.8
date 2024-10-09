09296c7ce8edbe23f76e9be44d6d343892b31ba2
YvesPa <166829028+YvesPa@users.noreply.github.com>
2024-10-09 14:36:35 +0200

    Correction sub-infos

--- src/Hean.ts
+++ src/Hean.ts
@@ -24,7 +24,7 @@ import {
     HeanTag
 } from './HeanInterfaces'
 
-const BASE_VERSION = '1.0.0'
+const BASE_VERSION = '1.0.1'
 export const getExportVersion = (EXTENSION_VERSION: string): string => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.')
 }
