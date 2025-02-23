759c20478ff8d7f757f4e1fe5db35f726e46d3b4
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2025-02-23 17:28:07 +0100

    Update PlatinumScans.ts

--- src/PlatinumScans/PlatinumScans.ts
+++ src/PlatinumScans/PlatinumScans.ts
@@ -13,7 +13,7 @@ import {
 const DOMAIN = 'https://platinumscans.com'
 
 export const PlatinumScansInfo: SourceInfo = {
-    version: getExportVersion('0.0.0'),
+    version: getExportVersion('0.0.1'),
     name: 'PlatinumScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
@@ -28,6 +28,8 @@ export const PlatinumScansInfo: SourceInfo = {
 export class PlatinumScans extends Madara {
 
     baseUrl: string = DOMAIN
+
+    override chapterEndpoint = 1
 
     override hasAdvancedSearchPage = false
 }
