eb2f9e374f82e16e1b97ea15629646035c9822e9
Batmeow <segagfx@gmail.com>
2025-02-23 22:16:31 -0800

    Update Tag sorting on Mangakakalot to match other instances.

--- src/Mangakakalot/Mangakakalot.ts
+++ src/Mangakakalot/Mangakakalot.ts
@@ -167,8 +167,8 @@ export class Mangakakalot extends MangaBox {
         }
 
         tags.sort((a, b) => {
-            if (a.label > b.label) { return 1 }
-            if (a.label < b.label) { return -1 }
+            if (a.label > b.label) return 1
+            if (a.label < b.label) return -1
             return 0
         })
 
