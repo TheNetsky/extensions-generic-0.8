92a10132e997c830ca43f02af57c6d5770f21bb9
MaosyBaos <106411641+MaosyBaos@users.noreply.github.com>
2025-03-03 23:35:27 +0000

    Update CypherScans.ts

--- src/CypherScans/CypherScans.ts
+++ src/CypherScans/CypherScans.ts
@@ -10,10 +10,10 @@ import {
     MangaStream
 } from '../MangaStream'
 
-const DOMAIN = 'https://cypher-scans.xyz'
+const DOMAIN = 'https://cypheroscans.xyz'
 
 export const CypherScansInfo: SourceInfo = {
-    version: getExportVersion('0.0.1'),
+    version: getExportVersion('0.0.2'),
     name: 'CypherScans',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
