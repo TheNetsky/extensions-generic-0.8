c710b9f34bd2cc538482d5f1cbcfc232d044cfa8
MaosyBaos <106411641+MaosyBaos@users.noreply.github.com>
2025-05-12 23:44:45 +0100

    Update ResetScans.ts

--- src/ResetScans/ResetScans.ts
+++ src/ResetScans/ResetScans.ts
@@ -10,7 +10,7 @@ import {
     Madara
 } from '../Madara'
 
-const DOMAIN = 'https://rspro.xyz'
+const DOMAIN = 'https://reset-scans.org'
 
 export const ResetScansInfo: SourceInfo = {
     version: getExportVersion('0.0.9'),
