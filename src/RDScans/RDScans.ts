dd4ac0306b71e016ce1b324103a150d799b70873
drewjc75 <23193423+drewjc75@users.noreply.github.com>
2025-10-08 10:25:30 -0400

    Added RDScans.com

---
+++ src/RDScans/RDScans.ts
@@ -0,0 +1,31 @@
+import {
+    ContentRating,
+    SourceInfo,
+    BadgeColor,
+    SourceIntents
+} from '@paperback/types'
+
+import {
+    getExportVersion,
+    Madara
+} from '../Madara'
+
+const DOMAIN = 'https://rdscans.com'
+
+export const RDScansInfo: SourceInfo = {
+    version: getExportVersion('0.0.0'),
+    name: 'RDScans',
+    description: `Extension that pulls manga from ${DOMAIN}`,
+    author: 'Netsky',
+    authorWebsite: 'https://github.com/TheNetsky',
+    icon: 'icon.png',
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [],
+    intents: SourceIntents.MANGA_CHAPTERS | SourceIntents.HOMEPAGE_SECTIONS | SourceIntents.CLOUDFLARE_BYPASS_REQUIRED | SourceIntents.SETTINGS_UI
+}
+
+export class RDScans extends Madara {
+
+    baseUrl: string = DOMAIN
+}
