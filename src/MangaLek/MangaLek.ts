9d7a8d3da0797d5f29f2be82da7a9436ed8ea71a
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2026-01-27 23:11:57 +0100

    Revert "MangaLek Domain Change (#154)"
    This reverts commit 64db755ea89b0b5b322679d0f1597be21ba37727.

--- src/MangaLek/MangaLek.ts
+++ src/MangaLek/MangaLek.ts
@@ -12,10 +12,10 @@ import {
 
 import { MangaLekParser } from './MangaLekParser'
 
-const DOMAIN = 'https://lek-manga.net'
+const DOMAIN = 'https://lekmanga.net'
 
 export const MangaLekInfo: SourceInfo = {
-    version: getExportVersion('0.0.6'),
+    version: getExportVersion('0.0.5'),
     name: 'MangaLek',
     description: `Extension that pulls manga from ${DOMAIN}`,
     author: 'Netsky',
